2a1881cda34625168a4da62ece781794a932f990
Kai-Xuan <chenkx.jsh@aliyun.com>
2023-03-16 02:52:52 +0000

    upload tinyimagenet

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,7 @@
 import time
 from .cifar10 import load_cifar10
 from .cifar100 import load_cifar100
+from .tinyimagenet import load_tinyimagenet
 
 def load_dataset(root, name, image_size, 
                  train_batch_size=64, valid_batch_size=64, 
@@ -27,4 +28,13 @@ def load_dataset(root, name, image_size,
                             rank=rank,
                             seed=seed
                             ) 
-    return 0
+    if name.lower() == 'tinyimagenet':
+        return load_tinyimagenet(root=root,  
+                            image_size=image_size,  
+                            train_batch_size=train_batch_size,
+                            valid_batch_size=valid_batch_size,
+                            distribute=distribute,
+                            split=split,
+                            rank=rank,
+                            seed=seed
+                            ) 
---
+++ scripts/tinyimage_csgd.sh
@@ -0,0 +1,22 @@
+
+# TinyImageNet training 100000
+# 50000/(512*16) = 12.20
+# 50000/(64*16)  = 96.16
+
+## AlexNet
+python main.py --dataset_name "TinyImageNet" --image_size 64 --batch_size 64 --mode "csgd" --size 16 --lr 0.1 --model "AlexNet_M" --warmup_step 0 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
+python main.py --dataset_name "TinyImageNet" --image_size 64 --batch_size 512 --mode "csgd" --size 16 --lr 0.8 --model "AlexNet_M" --warmup_step 0 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
+
+# training with amp（automatic mixed precision）
+python main.py --dataset_name "TinyImageNet" --image_size 64 --batch_size 64 --mode "csgd" --size 16 --lr 0.1 --model "AlexNet_M" --warmup_step 0 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0 --amp
+python main.py --dataset_name "TinyImageNet" --image_size 64 --batch_size 512 --mode "csgd" --size 16 --lr 0.8 --model "AlexNet_M" --warmup_step 0 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0 --amp
+
+
+## ResNet18
+python main.py --dataset_name "TinyImageNet" --image_size 56 --batch_size 64 --mode "csgd" --size 16 --lr 0.1 --model "ResNet18_M" --warmup_step 120 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
+python main.py --dataset_name "TinyImageNet" --image_size 56 --batch_size 512 --mode "csgd" --size 16 --lr 0.8 --model "ResNet18_M" --warmup_step 120 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
+
+# training with amp（automatic mixed precision）
+python main.py --dataset_name "TinyImageNet" --image_size 56 --batch_size 64 --mode "csgd" --size 16 --lr 0.1 --model "ResNet18_M" --warmup_step 120 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0 --amp
+python main.py --dataset_name "TinyImageNet" --image_size 56 --batch_size 512 --mode "csgd" --size 16 --lr 0.8 --model "ResNet18_M" --warmup_step 120 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0 --amp
+
