c21145b8678408f1824cdab260f51ef05d76cb5e
Kai-Xuan <chenkx.jsh@aliyun.com>
2023-03-30 06:29:07 +0000

    update tiny imagenet

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,7 +2,6 @@
 
 import time
 from .cifar10 import load_cifar10
-from .cifar100 import load_cifar100
 from .tinyimagenet import load_tinyimagenet
 
 def load_dataset(root, name, image_size, 
@@ -10,16 +9,6 @@ def load_dataset(root, name, image_size,
                  distribute=False, split=None, rank=0, seed=666):
     if name.lower() == 'cifar10':
         return load_cifar10(root=root,  
-                            image_size=image_size,  
-                            train_batch_size=train_batch_size,
-                            valid_batch_size=valid_batch_size,
-                            distribute=distribute,
-                            split=split,
-                            rank=rank,
-                            seed=seed
-                            ) 
-    if name.lower() == 'cifar100':
-        return load_cifar100(root=root,  
                             image_size=image_size,  
                             train_batch_size=train_batch_size,
                             valid_batch_size=valid_batch_size,
--- scripts/cifar100_ring.sh
+++
@@ -1,8 +0,0 @@
-
-# Resnet18
-python main.py --dataset_name "CIFAR100" --image_size 56 --batch_size 64 --mode "ring" --size 16  --lr 0.1 --model "ResNet18_M"  --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
-python main.py --dataset_name "CIFAR100" --image_size 56 --batch_size 512 --mode "ring" --size 16  --lr 0.8 --model "ResNet18_M"  --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
-
-
-python main.py --dataset_name "CIFAR100" --image_size 56 --batch_size 64 --mode "ring" --shuffle 'random' --size 16  --lr 0.1 --model "ResNet18_M"  --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
-python main.py --dataset_name "CIFAR100" --image_size 56 --batch_size 512 --mode "ring" --shuffle 'random' --size 16  --lr 0.8 --model "ResNet18_M"  --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
