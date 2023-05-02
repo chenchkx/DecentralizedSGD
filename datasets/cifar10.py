8d2bf0ea655c2b51eb8c8118f05508459ea990a4
chenchkx <chenkx.jsh@aliyun.com>
2023-05-02 06:29:44 +0000

    reverse cifar10

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -8,30 +8,20 @@ from .distribute_dataset import distribute_dataset
 def load_cifar10(root, transforms=None, image_size=32,  
                  train_batch_size=64, valid_batch_size=64,
                  distribute=False, split=1.0, rank=0, seed=666):
-    # 对训练集进行数据增强
-    transform_train = tfs.Compose([
-        tfs.Resize((image_size,image_size)),
-        # 随机水平翻转
-        tfs.RandomHorizontalFlip(),
-        # 以0.5的概率随机垂直翻转
-        tfs.RandomVerticalFlip(p=0.5),
-        # 将图片转换为tensor格式
-        tfs.ToTensor(),
-        # 对RGB三个通道的像素值进行归一化，mean和std分别为CIFAR10的均值和标准差
-        tfs.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-
-    # 对测试集进行数据处理，不进行数据增强
-    transform_test = tfs.Compose([
-        tfs.Resize((image_size,image_size)),
-        tfs.ToTensor(),
-        tfs.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    
+    if transforms is None:
+        transforms = tfs.Compose([
+            tfs.Resize((image_size,image_size)),
+            tfs.ToTensor(),
+            tfs.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        ])
 
     if train_batch_size is None:
         train_batch_size = 1
     if split is None:
         split = [1.0]
-    train_set = CIFAR10(root, True, transform_train, download=True)
-    valid_set = CIFAR10(root, False, transform_test, download=True)
+    train_set = CIFAR10(root, True, transforms, download=True)
+    valid_set = CIFAR10(root, False, transforms, download=True)
     if distribute:
         train_set = distribute_dataset(train_set, split, rank, seed=seed)
     train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True, drop_last=True)
