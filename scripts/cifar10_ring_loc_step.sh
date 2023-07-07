8abf43ddb01620503508d31b022c4de91f3c8d03
chenchkx <chenkx.jsh@aliyun.com>
2023-07-07 05:43:20 +0000

    add type1 scripts

--- scripts/cifar10_ring_loc_step.sh
+++ scripts/cifar10_ring_loc_step.sh
@@ -11,7 +11,7 @@ python main.py --dataset_name "CIFAR10" --image_size 64 --batch_size 512 --mode 
 ## ResNet18_M
 python main_test.py --dataset_name "CIFAR10" --image_size 56 --batch_size 64 --mode "ring" --size 16 --lr 0.1 --model "ResNet18_M" --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
 python main_test.py --dataset_name "CIFAR10" --image_size 56 --batch_size 512 --mode "ring" --size 16 --lr 0.8 --model "ResNet18_M" --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0 --seed 777 --affine_type 'None'
-
+python main_test.py --dataset_name "CIFAR10" --image_size 56 --batch_size 512 --mode "ring" --size 16 --lr 0.8 --model "ResNet18_M" --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0 --seed 777 --affine_type 'type1' --loc_step=20
 ## ResNet34_M
 python main.py --dataset_name "CIFAR10" --image_size 56 --batch_size 64 --mode "ring" --size 16 --lr 0.1 --model "ResNet34_M" --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
 python main.py --dataset_name "CIFAR10" --image_size 56 --batch_size 512 --mode "ring" --size 16 --lr 0.8 --model "ResNet34_M" --warmup_step 60 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --pretrained 1 --device 0
